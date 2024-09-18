2ca638356c2b1d068e12497320eccf1df2078829
Gourav Choudhary <gouravcc1@gmail.com>
2024-09-18 17:28:54 +0530

    added logs

--- src/pages/api/ask-ai.js
+++ src/pages/api/ask-ai.js
@@ -34,7 +34,8 @@ export default async function handler(req, res) {
                 return res.status(200).json({ success: true, data: data });
             } catch (error) {
                 // Return error response
-                return res.status(400).json({ success: false, error: error.message });
+                console.error("error in chatBot api ",error)
+                return res.status(400).json({ success: false, error: error });
             }
         default:
             // Handle unsupported request methods
